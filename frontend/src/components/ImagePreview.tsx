3626c19914e044947bc16934e8895044b6a94248
mericanAncer <vicenteputheany701@gmail.com>
2024-12-13 15:09:10 -0600

    backend link fixed

--- frontend/src/components/ImagePreview.tsx
+++ frontend/src/components/ImagePreview.tsx
@@ -6,7 +6,7 @@ import './ImagePreview.css';
 // If using the utility file, uncomment the next line
 // import { downloadImage } from '../utils/downloadUtils';
 
-const baseUrl = `https://legendary-umbrella.onrender.com:5000`;
+const baseUrl = `https://legendary-umbrella.onrender.com`;
 
 interface ImageData {
   url: string;
